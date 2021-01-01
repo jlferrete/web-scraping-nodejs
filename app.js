6a896322b9f74a96196dcf720b491af6e0cbb864
Jlferrete <joseluisferrete@gmail.com>
2021-01-01 21:08:48 +0100

    Basic function and arrays

---
+++ app.js
@@ -0,0 +1,19 @@
+const request = require("request");
+const requestPromise = require("request-promise");
+const cheerio = require("cheerio");
+
+//Arrays
+
+let empresasArray = [];
+let paginationArray = [];
+let resultObject = [];
+
+//Function
+
+(async() => {
+    try {
+
+    } catch (error) {
+        console.error(error);
+    }
+})();
