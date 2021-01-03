322d0005aa53bd2aa76d911f7bb82048dc2a41e4
Jlferrete <joseluisferrete@gmail.com>
2021-01-03 10:15:47 +0100

    First request and cheerio

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ let resultObject = [];
 
 (async() => {
     try {
+        //get request para traernos el html
+        let response = await requestPromise('https://chileservicios.com/industrias/tecnologias-de-la-informacion/');
+        let $ = cheerio.load(response);
+        const title = $("title").text();
+        console.log(title);
+        //seleccionar el último link de la paginación
+
+
 
     } catch (error) {
         console.error(error);
